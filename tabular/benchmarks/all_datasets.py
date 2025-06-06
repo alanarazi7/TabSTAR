f3238d46a487cf1c583666e100adf99cd2c76f4e
alanarazi7 <alanarazi7@gmail.com>
2025-06-06 16:25:57 +0300

    out

--- tabular/benchmarks/all_datasets.py
+++ tabular/benchmarks/all_datasets.py
@@ -11,7 +11,6 @@ from tabular.datasets.tabular_datasets import OpenMLDatasetID, KaggleDatasetID, 
 
 # Datasets with too many features
 TOO_MANY_FEATURES = [
-    OpenMLDatasetID.BIN_ANONYM_ARCENE,                               # 10000
     OpenMLDatasetID.BIN_ANONYM_BIORESPONSE,                          # 1776
     OpenMLDatasetID.BIN_ANONYM_CHRISTINE,                            # 1636
     OpenMLDatasetID.BIN_ANONYM_GINA,                                 # 970
