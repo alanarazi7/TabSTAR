4a65b476da9a37fc24f9f4c515f5a54eff0c345e
alanarazi7 <alanarazi7@gmail.com>
2025-06-15 14:15:33 +0300

    fix file

--- tabstar_paper/benchmark_testing.py
+++ tabstar_paper/benchmark_testing.py
@@ -5,14 +5,12 @@ from tabstar_paper.do_benchmark import eval_tabstar_on_dataset
 from tabular.benchmarks.all_datasets import TEXTUAL_DATASETS, TEXTUAL_BIG
 from tabular.utils.io_handlers import dump_json
 
-tuples = [(dataset, run_num) for dataset in set(TEXTUAL_DATASETS).difference(TEXTUAL_BIG) for run_num in range(10)]
-shuffle(tuples)
-
-for dataset, run_num in tuples:
-    key_file = f"benchmark_results/{dataset.name}_{run_num}.txt"
-    if os.path.exists(key_file):
-        continue
-    print(f"Evaluating {dataset.name} run {run_num}...")
-    metric = eval_tabstar_on_dataset(dataset_id=dataset, run_num=run_num, train_examples=10_000)
-    result = {"metric": metric, "dataset": dataset.name, "run_num": run_num}
-    dump_json(result, key_file)
+for dataset in sorted(set(TEXTUAL_DATASETS).difference(TEXTUAL_BIG)):
+    for run_num in range(10):
+        key_file = f"benchmark_results/{dataset.name}_{run_num}.txt"
+        if os.path.exists(key_file):
+            continue
+        print(f"Evaluating {dataset.name} run {run_num}...")
+        metric = eval_tabstar_on_dataset(dataset_id=dataset, run_num=run_num, train_examples=10_000)
+        result = {"metric": metric, "dataset": dataset.name, "run_num": run_num}
+        dump_json(result, key_file)
