eac7c17113d871cde236baf4025e5d29d215e3a5
alanarazi7 <alanarazi7@gmail.com>
2025-06-16 16:49:30 +0300

    catboost

--- tabstar_paper/benchmark_testing.py
+++ tabstar_paper/benchmark_testing.py
@@ -13,12 +13,12 @@ for dataset in TEXTUAL_DATASETS:
         # Skip for now big datasets, we need to handle the num_examples
         continue
     for run_num in range(10):
-        key_file = f"benchmark_results/tabstar_{dataset.name}_{run_num}.txt"
-        if not os.path.exists(key_file):
-            print(f"Evaluating {dataset.name} run {run_num}...")
-            metric = eval_tabstar_on_dataset(dataset_id=dataset, run_num=run_num, train_examples=10_000)
-            result = {"metric": metric, "dataset": dataset.name, "run_num": run_num}
-            dump_json(result, key_file)
+        # key_file = f"benchmark_results/tabstar_{dataset.name}_{run_num}.txt"
+        # if not os.path.exists(key_file):
+        #     print(f"Evaluating {dataset.name} run {run_num}...")
+        #     metric = eval_tabstar_on_dataset(dataset_id=dataset, run_num=run_num, train_examples=10_000)
+        #     result = {"metric": metric, "dataset": dataset.name, "run_num": run_num}
+        #     dump_json(result, key_file)
         for model in BASELINES:
             key_file = f"benchmark_results/{model.SHORT_NAME}_{dataset.name}_{run_num}.txt"
             if not os.path.exists(key_file):
