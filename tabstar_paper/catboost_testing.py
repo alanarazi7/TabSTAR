d82e2b139c13c6f99da5354a64360ddb189375b7
alanarazi7 <alanarazi7@gmail.com>
2025-06-21 00:37:59 +0300

    devices

--- tabstar_paper/catboost_testing.py
+++ tabstar_paper/catboost_testing.py
@@ -2,18 +2,15 @@ import os
 
 from tabstar_paper.baselines.catboost import CatBoost
 from tabstar_paper.do_baseline import eval_baseline_on_dataset
-from tabular.benchmarks.all_datasets import TEXTUAL_DATASETS, TEXTUAL_BIG
+from tabular.benchmarks.all_datasets import TEXTUAL_DATASETS
 from tabular.utils.io_handlers import dump_json
 
 datasets = sorted(TEXTUAL_DATASETS, key=lambda d: d.name)
-# Skip regression datasets for now, we only evaluate classification
-datasets = [d for d in datasets if not d.name.startswith('REG_')]
-
 
 for dataset in datasets:
     for run_num in range(10):
         for model in [CatBoost]:
-            key_file = f"benchmark_results_v4/{model.SHORT_NAME}_{dataset.name}_{run_num}.txt"
+            key_file = f"benchmark_results_v6/{model.SHORT_NAME}_{dataset.name}_{run_num}.txt"
             if not os.path.exists(key_file):
                 print(f"Evaluating {model.SHORT_NAME} on {dataset.name} run {run_num}...")
                 metric = eval_baseline_on_dataset(model=model, dataset_id=dataset, run_num=run_num, train_examples=10_000)
