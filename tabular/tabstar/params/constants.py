bc79913924a34d98e500cdf2265e3f0162710889
alanarazi7 <alanarazi7@gmail.com>
2025-06-21 23:38:22 +0300

    Pretraining: hardcoding the folds

--- tabular/tabstar/params/constants.py
+++ tabular/tabstar/params/constants.py
@@ -10,6 +10,7 @@ E5_SMALL = 'intfloat/e5-small-v2'
 D_MODEL = 384
 E5_LAYERS = 12
 
+# TODO: this should be part of a branch of experiments.
 class NumberVerbalization(StrEnum):
     NONE = 'none'
     RANGE = 'range'
