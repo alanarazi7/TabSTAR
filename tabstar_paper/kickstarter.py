0422bbc12024a60958247d8db03ae92548b84f6a
alanarazi7 <alanarazi7@gmail.com>
2025-06-21 16:39:00 +0300

    verbose

--- tabstar_paper/kickstarter.py
+++ tabstar_paper/kickstarter.py
@@ -4,5 +4,6 @@ from tabstar_paper.do_benchmark import eval_tabstar_on_dataset
 
 run_num= 0
 dataset = OpenMLDatasetID.BIN_PROFESSIONAL_KICKSTARTER_FUNDING
-metric = eval_tabstar_on_dataset(dataset_id=dataset, run_num=run_num, train_examples=10_000)
+metric = eval_tabstar_on_dataset(dataset_id=dataset, run_num=run_num, train_examples=10_000,
+                                 verbose=True)
 print(f"Scored {metric:.4f} on dataset {dataset.name} run {run_num}.")
