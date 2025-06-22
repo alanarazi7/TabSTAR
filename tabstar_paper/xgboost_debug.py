58482c5825d98af965080c12c6401806705bd403
alanarazi7 <alanarazi7@gmail.com>
2025-06-22 22:48:09 +0300

    debugging

---
+++ tabstar_paper/xgboost_debug.py
@@ -0,0 +1,20 @@
+import os
+
+from tabstar_paper.baselines.catboost import CatBoost
+from tabstar_paper.baselines.xgboost import XGBoost
+from tabstar_paper.do_baseline import eval_baseline_on_dataset
+from tabular.benchmarks.all_datasets import TEXTUAL_DATASETS
+from tabular.utils.io_handlers import dump_json
+
+datasets = sorted(TEXTUAL_DATASETS, key=lambda d: d.name)
+datasets = [d for d in datasets if not d.name.startswith('REG_')]
+
+for dataset in datasets:
+    for run_num in range(10):
+        for model in [XGBoost]:
+            key_file = f"benchmark_result/{model.SHORT_NAME}_{dataset.name}_{run_num}.txt"
+            if not os.path.exists(key_file):
+                print(f"Evaluating {model.SHORT_NAME} on {dataset.name} run {run_num}...")
+                metric = eval_baseline_on_dataset(model=model, dataset_id=dataset, run_num=run_num, train_examples=10_000)
+                result = {"metric": metric, "dataset": dataset.name, "run_num": run_num}
+                dump_json(result, key_file)
