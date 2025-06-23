26c92e7a28ef4690edf6b2429b15d4696d1831e4
alanarazi7 <alanarazi7@gmail.com>
2025-06-23 21:43:04 +0300

    Benchmark scripts

--- tabstar_paper/do_benchmark.py
+++ tabstar_paper/do_benchmark.py
@@ -1,7 +1,5 @@
 import torch
 
-from tabstar_paper.baselines.catboost import CatBoost
-from tabstar_paper.do_baseline import eval_baseline_on_dataset
 from tabstar_paper.do_tabstar import eval_tabstar_on_dataset
 
 # TODO: these imports shouldn't exist, they are from the tabular repo
@@ -11,7 +9,6 @@ from tabular.utils.io_handlers import dump_json
 import os
 
 datasets = sorted(TEXTUAL_DATASETS, key=lambda d: d.name)
-datasets = [d for d in datasets if not d.name.startswith('REG_')]
 runs = [(dataset, run_num) for dataset in datasets for run_num in range(10)]
 GPU = os.getenv("GPU", None)
 device = GPU
@@ -29,10 +26,3 @@ for dataset, run_num in runs:
     metric = eval_tabstar_on_dataset(dataset_id=dataset, run_num=run_num, train_examples=10_000, device=device)
     result = {"metric": metric, "dataset": dataset.name, "run_num": run_num}
     dump_json(result, key_file)
-    # for model in [CatBoost]:
-    #     key_file = f".benchmark_results/{model.SHORT_NAME}_{dataset.name}_{run_num}.txt"
-    #     if not os.path.exists(key_file):
-    #         print(f"Evaluating {model.SHORT_NAME} on {dataset.name} run {run_num}...")
-    #         metric = eval_baseline_on_dataset(model=model, dataset_id=dataset, run_num=run_num, train_examples=10_000)
-    #         result = {"metric": metric, "dataset": dataset.name, "run_num": run_num}
-    #         dump_json(result, key_file)
---
+++ tabstar_paper/do_catboost.py
@@ -0,0 +1,20 @@
+from tabstar_paper.baselines.catboost import CatBoost
+from tabstar_paper.do_baseline import eval_baseline_on_dataset
+
+# TODO: these imports shouldn't exist, they are from the tabular repo
+from tabular.benchmarks.all_datasets import TEXTUAL_DATASETS
+from tabular.utils.io_handlers import dump_json
+
+import os
+
+datasets = sorted(TEXTUAL_DATASETS, key=lambda d: d.name)
+runs = [(dataset, run_num) for dataset in datasets for run_num in range(10)]
+
+for dataset, run_num in runs:
+    for model in [CatBoost]:
+        key_file = f".benchmark_results/{model.SHORT_NAME}_{dataset.name}_{run_num}.txt"
+        if not os.path.exists(key_file):
+            print(f"Evaluating {model.SHORT_NAME} on {dataset.name} run {run_num}...")
+            metric = eval_baseline_on_dataset(model=model, dataset_id=dataset, run_num=run_num, train_examples=10_000)
+            result = {"metric": metric, "dataset": dataset.name, "run_num": run_num}
+            dump_json(result, key_file)
