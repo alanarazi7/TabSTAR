bcfcbd6130f9895ffb2c4a39e896e6587a33e767
alanarazi7 <alanarazi7@gmail.com>
2025-06-28 22:33:45 +0300

    fix

--- src/tabstar/preprocessing/scaler.py
+++ src/tabstar/preprocessing/scaler.py
@@ -9,19 +9,17 @@ Z_MAX_ABS_VAL = 3
 def fit_standard_scaler(s: Series) -> StandardScaler:
     s = s.copy().dropna()
     scaler = StandardScaler()
-    s = s.values.reshape(-1, 1)
-    clipped = s.clip(-Z_MAX_ABS_VAL, Z_MAX_ABS_VAL)
-    scaler.fit(clipped)
+    scaler.fit(s.values.reshape(-1, 1))
     return scaler
 
 def transform_clipped_z_scores(s: Series, scaler: StandardScaler, allow_null: bool = False) -> Series:
-    invalid = get_invalid_indices(s)
+    # invalid = get_invalid_indices(s)
     s = s.copy()
-    if allow_null:
-        s = s.fillna(s.mean())
+    # if allow_null:
+    #     s = s.fillna(s.mean())
     s_val = scaler.transform(s.values.reshape(-1, 1)).flatten()
     s_val = s_val.clip(-Z_MAX_ABS_VAL, Z_MAX_ABS_VAL)
-    if invalid:
-        s_val[sorted(invalid)] = 0
+    # if invalid:
+    #     s_val[sorted(invalid)] = 0
     s = Series(s_val, index=s.index, name=s.name)
     return s
