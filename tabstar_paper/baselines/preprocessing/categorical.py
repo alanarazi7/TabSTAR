d735e855c9e66a3a09e2eace1c80596bea834cb7
alanarazi7 <alanarazi7@gmail.com>
2025-06-30 01:06:26 +0300

    XGBoost: removing mode fillers, as we anyway fill missing values with an unknown value during curation

--- tabstar_paper/baselines/preprocessing/categorical.py
+++ tabstar_paper/baselines/preprocessing/categorical.py
@@ -1,25 +1,6 @@
-from dataclasses import dataclass
-from typing import Optional
 from pandas import Series
 from sklearn.preprocessing import OrdinalEncoder
-import numpy as np
 
-@dataclass
-class ModeFiller:
-    src: Series
-    target: Series
-    mode: object
-
-def fill_mode(x_train: Series, x_test: Optional[Series] = None) -> ModeFiller:
-    """Fill the test set with the mode of the train set."""
-    train_mode = x_train.mode(dropna=True)
-    mode_value = train_mode.iloc[0] if not train_mode.empty else np.nan
-    x_train_filled = x_train.copy().fillna(mode_value)
-    if x_test is not None:
-        x_test_filled = x_test.copy().fillna(mode_value)
-    else:
-        x_test_filled = None
-    return ModeFiller(src=x_train_filled, target=x_test_filled, mode=mode_value)
 
 class CategoricalEncoder:
     def __init__(self):
