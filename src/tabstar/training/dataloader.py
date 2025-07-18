7210fa156e049e435e3a9ab6beb1130296b7215f
alanarazi7 <alanarazi7@gmail.com>
2025-07-18 14:48:49 +0300

    dataloader

--- src/tabstar/training/dataloader.py
+++ src/tabstar/training/dataloader.py
@@ -1,13 +1,10 @@
 import numpy as np
 import pandas as pd
-from torch import Tensor
-from torch.utils.data import DataLoader
+import torch
+from torch.utils.data import Dataset, DataLoader
 
 from tabstar.arch.config import BATCH_SIZE
-
-
-from torch.utils.data import Dataset
-
+from tabstar.preprocessing.tokenization import tokenize
 from tabstar.tabstar_verbalizer import TabSTARData
 
 
@@ -15,40 +12,51 @@ class TabSTARDataset(Dataset):
     def __init__(self, data: TabSTARData):
         self.x_txt = data.x_txt
         self.x_num = data.x_num
-        if isinstance(data.y, pd.Series):
-            self.y = data.y.reset_index(drop=True)
-        elif isinstance(data.y, np.ndarray):
-            self.y = pd.Series(data.y, dtype=np.float32)
-        else:
-            # Dummy target for convenience
-            self.y = pd.Series(np.zeros(len(data.x_txt)), dtype=np.float32)
         self.d_output = data.d_output
+        dtype = torch.float32 if self.d_output == 1 else torch.int64
+        if isinstance(data.y, pd.Series):
+            self.y = torch.tensor(data.y.values, dtype=dtype)
+        elif isinstance(data.y, np.ndarray):
+            self.y = torch.tensor(data.y, dtype=dtype)
+        else:
+            self.y = torch.zeros(len(data.x_txt), dtype=dtype)
 
     def __len__(self):
         return len(self.y)
 
     def __getitem__(self, idx: int):
+        # x_txt stays as string (tokenize in collate_fn)
         x_txt = self.x_txt[idx]
-        x_num = self.x_num[idx]
-        y = self.y.iloc[idx]
+        x_num = torch.tensor(self.x_num[idx], dtype=torch.float32)
+        y = self.y[idx]
         return x_txt, x_num, y, self.d_output
-    
+
+
 def get_dataloader(data: TabSTARData, is_train: bool, batch_size: int = BATCH_SIZE) -> DataLoader:
     dataset = TabSTARDataset(data)
     return DataLoader(dataset, shuffle=is_train, batch_size=batch_size, num_workers=0, collate_fn=collate_fn)
 
 
-def collate_fn(batch) -> TabSTARData:
+def collate_fn(batch):
     x_txt_batch, x_num_batch, y_batch, d_output_batch = zip(*batch)
-    # Assuming all batches have the same d_output, which is correct for finetune
-    assert isinstance(x_txt_batch, Tensor)
-    assert isinstance(x_num_batch, Tensor)
-    assert isinstance(y_batch, Tensor)
-    tokenize_in_collate = False
-    assert tokenize_in_collate, "Tokenization should be done in the collate function."
+    y = torch.stack(y_batch)
+    x_num = torch.stack(x_num_batch)
     d_output = d_output_batch[0]
-    x_txt = np.stack(x_txt_batch)
-    x_num = np.stack(x_num_batch)
-    y = pd.Series(y_batch)
-    data = TabSTARData(d_output=d_output, x_txt=x_txt, x_num=x_num, y=y)
-    return data
+    assert set(d_output_batch) == {d_output}, "All items in the batch must have the same d_output value"
+
+    # For x_txt, we convert to indices and tokenize the unique texts
+    x_txt_flat = np.array(x_txt_batch).reshape(-1)
+    unique_texts, inverse_indices = np.unique(x_txt_flat, return_inverse=True)
+    x_txt_indices = inverse_indices.reshape(len(x_txt_batch), -1)
+    x_txt_indices = torch.tensor(x_txt_indices, dtype=torch.long)
+    assert x_txt_indices.shape == x_num.shape
+
+    tokenized = tokenize(texts=list(unique_texts))
+
+    return {
+        'x_txt_indices': x_txt_indices,
+        'x_txt_tokenized': tokenized,
+        'x_num': x_num,
+        'y': y,
+        'd_output': d_output
+    }
