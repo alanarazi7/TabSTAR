2ed03751f50a93f91900d5b85e98ed29bdbdd546
alanarazi7 <alanarazi7@gmail.com>
2025-08-14 22:17:20 +0300

    Optuna: adding initial base class

--- tabstar_paper/constants.py
+++ tabstar_paper/constants.py
@@ -9,6 +9,7 @@ WANDB_ENTITY = os.getenv("WANDB_ENTITY")
 TABPFN_TOKEN = os.getenv("TABPFN_TOKEN")
 HF_TOKEN = os.getenv("HF_TOKEN")
 PRETRAIN_BATCH_SIZE = int(os.getenv("PRETRAIN_BATCH_SIZE", 32))
+TIME_BUDGET = int(os.getenv("TIME_BUDGET", 60 * 60 * 4))
 GPU = os.getenv("GPU")
 CPU = os.getenv("CPU") is not None
 
