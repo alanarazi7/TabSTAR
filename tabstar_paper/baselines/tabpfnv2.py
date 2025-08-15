b5900ab54ae8653e973622d65540e00bf5803288
alanarazi7 <alanarazi7@gmail.com>
2025-08-15 21:54:12 +0300

    TabPFN: reverting to paper version for reproducibility

--- tabstar_paper/baselines/tabpfnv2.py
+++ tabstar_paper/baselines/tabpfnv2.py
@@ -1,39 +1,32 @@
-from typing import Tuple
-
 from pandas import DataFrame, Series
 
 from tabstar.constants import SEED
 from tabstar_paper.baselines.abstract_model import TabularModel
-from tabpfn_client import TabPFNClassifier, TabPFNRegressor, set_access_token
+from tabpfn import TabPFNClassifier, TabPFNRegressor
 
-from tabstar_paper.constants import TABPFN_TOKEN
 
 MAX_SAMPLES = 10_000
+MAX_FEATURES = 500
 
 class TabPFNv2(TabularModel):
 
-    MODEL_NAME = "TabPFN-v2 🤯"
-    SHORT_NAME = "pfn"
+    MODEL_NAME = "TabPFNv2 🧪"
+    SHORT_NAME = "tabpfn"
     USE_VAL_SPLIT = False
     USE_MEDIAN_FILLING = False
     USE_CATEGORICAL_ENCODING = False
-    USE_TEXT_EMBEDDINGS = False
+    USE_TEXT_EMBEDDINGS = True
 
-    def initialize_model(self) -> TabPFNClassifier | TabPFNRegressor:
-        # TODO: Move away from closed-source client version, as this isn't reproducible, they improve the model
-        assert TABPFN_TOKEN, f"Please set the TABPFN_TOKEN environment variable to use TabPFN."
-        set_access_token(TABPFN_TOKEN)
-        model_cls = TabPFNRegressor if not self.is_cls else TabPFNClassifier
-        model = model_cls(random_state=SEED)
-        return model
+    def initialize_model(self) -> None:
+        return None
 
     def fit_model(self, x_train: DataFrame, y_train: Series, x_val: DataFrame, y_val: Series):
         if len(x_train) > MAX_SAMPLES:
             raise RuntimeError(f"TabPFN is not designed to handle datasets larger than {MAX_SAMPLES} samples. ")
+        if len(x_train.columns) > MAX_FEATURES:
+            print(f"🚨 Warning: {len(x_train.columns)} features detected, ignoring pretraining limits.")
+        cat_feature = [i for i, c in enumerate(x_train.columns) if c in self.categorical_features]
+        model_cls = TabPFNRegressor if not self.is_cls else TabPFNClassifier
+        self.model_ = model_cls(random_state=SEED, categorical_features_indices=cat_feature,
+                                ignore_pretraining_limits=True)
         self.model_.fit(x_train, y_train)
-
-    def fit_internal_preprocessor(self, x: DataFrame, y: Series):
-        pass
-
-    def transform_internal_preprocessor(self, x: DataFrame, y: Series) -> Tuple[DataFrame, Series]:
-        return x, y
--- tabstar_paper/constants.py
+++ tabstar_paper/constants.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 WANDB_API_KEY = os.getenv("WANDB_API_KEY")
 WANDB_ENTITY = os.getenv("WANDB_ENTITY")
-TABPFN_TOKEN = os.getenv("TABPFN_TOKEN")
 HF_TOKEN = os.getenv("HF_TOKEN")
 PRETRAIN_BATCH_SIZE = int(os.getenv("PRETRAIN_BATCH_SIZE", 32))
 TIME_BUDGET = int(os.getenv("TIME_BUDGET", 60 * 60 * 4))
