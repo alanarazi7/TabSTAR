a8022b6fcfcc0dd594c37abbe2a044a806154658
nathalie.hauser <ntl.hauser@gmail.com>
2025-08-17 22:35:12 +0300

    adding basic package test

---
+++ test/package/test_basic.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from tabstar.tabstar_model import TabSTARClassifier
+
+def test_tabstar_classifier_fit_predict():
+    # Create dummy data
+    X = pd.DataFrame({'a': [1, 2, 3, 4], 'b': [0, 1, 0, 1]})
+    y = [0, 1, 0, 1]
+
+    # Instantiate and fit
+    clf = TabSTARClassifier()
+    clf.fit(X, y)
+
+    # Predict
+    preds = clf.predict(X)
+    assert len(preds) == len(y)
