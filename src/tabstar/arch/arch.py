911f89d1850b41b7be3520da6f6cc79c32a0d4f0
alanarazi7 <alanarazi7@gmail.com>
2025-08-17 19:50:28 +0300

    TabSTAR arch - tokenization upfront

--- src/tabstar/arch/arch.py
+++ src/tabstar/arch/arch.py
@@ -1,4 +1,5 @@
-import numpy as np
+from typing import Dict
+
 import torch
 from torch import Tensor
 from transformers import AutoTokenizer, AutoModel, PreTrainedModel
@@ -23,8 +24,8 @@ class TabStarModel(PreTrainedModel):
         self.reg_head = PredictionHead()
         self.post_init()
 
-    def forward(self, x_txt: np.ndarray, x_num: np.ndarray, d_output: int) -> Tensor:
-        textual_embeddings = self.get_textual_embedding(x_txt)
+    def forward(self, x_txt: Tensor, x_num: Tensor, tokenized: Dict[str, Tensor], d_output: int) -> Tensor:
+        textual_embeddings = self.get_textual_embedding(x_txt, tokenized=tokenized)
         x_num = torch.tensor(x_num, dtype=textual_embeddings.dtype, device=textual_embeddings.device)
         embeddings = self.numerical_fusion(textual_embeddings=textual_embeddings, x_num=x_num)
         encoded = self.tabular_encoder(embeddings)
@@ -37,34 +38,30 @@ class TabStarModel(PreTrainedModel):
         assert tuple(target_scores.shape) == (x_txt.shape[0], d_output)
         return target_scores
 
-    def get_textual_embedding(self, x_txt: np.array) -> Tensor:
-        text_batch_size = 128
-        while text_batch_size > 1:
+    def get_textual_embedding(self, x_txt: Tensor, tokenized: Dict[str, Tensor]) -> Tensor:
+        text_batch = 128
+        unique_texts = tokenized['input_ids'].size(0)
+        x_txt_shape = x_txt.shape
+        while text_batch > 1:
             try:
-                return self.get_textual_embedding_in_batches(x_txt, text_batch_size=text_batch_size)
+                return self.get_textual_embedding_in_batches(x_txt, tokenized=tokenized, text_batch=text_batch)
             except torch.cuda.OutOfMemoryError:
-                text_batch_size //= 2
+                text_batch //= 2
                 clear_cuda_cache()
-                print(f"🤯 Reducing batch size to {text_batch_size} due to OOM")
-        raise RuntimeError(f"🤯 OOM even with batch size 1!")
+                print(f"🤯 Reduced text batch size to {text_batch} due to OOM for {x_txt_shape=}, {unique_texts=}")
+        raise RuntimeError(f"🤯 OOM even with batch size 1! Consider reducing the model's batch size")
 
-    def get_textual_embedding_in_batches(self, x_txt: np.array, text_batch_size: int) -> Tensor:
-        # Get unique texts and mapping indices
-        unique_texts, inverse_indices = np.unique(x_txt, return_inverse=True)
-        num_unique_texts = len(unique_texts)
+    def get_textual_embedding_in_batches(self, x_txt: Tensor, tokenized: Dict[str, Tensor], text_batch: int) -> Tensor:
+        num_unique_texts = tokenized['input_ids'].size(0)
         embeddings = []
-        for i in range(0, num_unique_texts, text_batch_size):
-            batch_texts = unique_texts[i:i + text_batch_size].tolist()
-            inputs = self.tokenizer(batch_texts, padding=True, return_tensors='pt', truncation=True)
-            inputs = {k: v.to(self.device) for k, v in inputs.items()}
-            outputs = self.text_encoder(**inputs)
+        for i in range(0, num_unique_texts, text_batch):
+            batch_inputs = {k: v[i:i + text_batch] for k, v in tokenized.items()}
+            outputs = self.text_encoder(**batch_inputs)
             # Take the [CLS] token representation
             embeddings.append(outputs.last_hidden_state[:, 0, :])
         embeddings = torch.cat(embeddings, dim=0)
-        inverse_indices = torch.tensor(inverse_indices, dtype=torch.long, device=embeddings.device)
-        # Map the unique embeddings back to the original positions and reshape to match the original dimension
         batch_size, seq_len = x_txt.shape
-        embeddings = embeddings[inverse_indices].view(batch_size, seq_len, -1)
+        embeddings = embeddings[x_txt]
         if not tuple(embeddings.shape) == (batch_size, seq_len, self.config.d_model):
             raise RuntimeError(f"Unexpected embedding shape: {embeddings.shape}")
         return embeddings
