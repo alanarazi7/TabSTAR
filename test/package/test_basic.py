e3537eb568335ab2c4e263b91b9a311f1578c42d
nathalie.hauser <ntl.hauser@gmail.com>
2025-08-17 22:44:02 +0300

    fixing test

--- test/package/test_basic.py
+++ test/package/test_basic.py
@@ -4,7 +4,7 @@ from tabstar.tabstar_model import TabSTARClassifier
 def test_tabstar_classifier_fit_predict():
     # Create dummy data
     X = pd.DataFrame({'a': [1, 2, 3, 4], 'b': [0, 1, 0, 1]})
-    y = [0, 1, 0, 1]
+    y = pd.Series([0, 1, 0, 1])
 
     # Instantiate and fit
     clf = TabSTARClassifier()
