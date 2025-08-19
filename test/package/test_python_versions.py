0e924e95a7c616db2fc2f3979a29afc88802055b
nathalie.hauser <ntl.hauser@gmail.com>
2025-08-19 22:40:12 +0300

    change to test

--- test/package/test_python_versions.py
+++ test/package/test_python_versions.py
@@ -1,12 +1,14 @@
 def test_python_version():
     """
-    should raise a syntax error in 3.9
-    should pass in 3.11
-    
+    match doesn't exists in 3.9.
+    this tests tries to use match.
+    if it's 3.11, it returns False to fail 
     """
     value_1 = 42
     value_2 = 42
-    match value_1:
-        case value_2:
-            assert True
-        
+    try: 
+        match value_1:
+            case value_2:
+                assert False # this code runs only if value_1 and value_2 match
+    except SyntaxError:
+        assert True # passes in 3.9 
