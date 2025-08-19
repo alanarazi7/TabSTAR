52c96e4f60c138a9e130ca0f7212d8c5c74a2d05
nathalie.hauser <ntl.hauser@gmail.com>
2025-08-19 21:52:07 +0300

    another test

--- test/package/test_python_versions.py
+++ test/package/test_python_versions.py
@@ -1,13 +1,7 @@
-from collections import OrderedDict
+import re
 
-def test_dictionary_and_ordered_dictionary_equality():
-    """
-    This test demonstrates a change in Python's behavior between versions 3.9 and 3.11.
-    In Python 3.9, a regular dictionary and an OrderedDict with the same contents are not considered equal.
-    In Python 3.11, they are considered equal. Therefore, this test will pass in Python 3.9 but fail in Python 3.11.
-    """
-    regular_dictionary = {'key_one': 1, 'key_two': 2}
-    ordered_dictionary = OrderedDict([('key_one', 1), ('key_two', 2)])
-    # In Python 3.9: regular_dictionary != ordered_dictionary
-    # In Python 3.11: regular_dictionary == ordered_dictionary if contents are the same
-    assert regular_dictionary != ordered_dictionary
+def test_regex_match_truthiness():
+    match = re.match(r"abc", "def")  # No match
+    # In Python 3.9: match is falsy (None)
+    # In Python 3.11: match is always truthy (if not None)
+    assert not match
