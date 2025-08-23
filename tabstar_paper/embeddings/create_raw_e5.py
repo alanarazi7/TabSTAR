64aeba2937d6acdd5f2d887f43d198fed197097c
alanarazi7 <alanarazi7@gmail.com>
2025-08-23 21:26:17 +0300

    create raw e5

--- tabstar_paper/embeddings/create_raw_e5.py
+++ tabstar_paper/embeddings/create_raw_e5.py
@@ -13,10 +13,10 @@ from tabstar_paper.preprocessing.sampling import subsample_dataset
 from tabstar_paper.preprocessing.text_embeddings import E5_CACHED_MODEL
 from tabstar_paper.utils.io_handlers import dump_json
 
-dataset_id = KaggleDatasetID.MUL_FOOD_YELP_REVIEWS
-col_name = 'text'
+dataset_id = OpenMLDatasetID.BIN_SOCIAL_IMDB_GENRE_PREDICTION
+col_name = 'Title'
 
-data = load_kaggle_dataset(dataset_id)
+data = load_openml_dataset(dataset_id)
 device = get_device()
 is_cls = True
 fold = 0
@@ -27,7 +27,7 @@ max_epochs = 50
 #     train_examples = 1_000
 #     max_epochs = 1
 x, y = subsample_dataset(x=data.x, y=data.y, is_cls=is_cls, train_examples=train_examples, fold=fold)
-x = x[[col_name]]
+# x = x[[col_name]]
 x_train, x_test, y_train, y_test = split_to_test(x=x, y=y, is_cls=is_cls, fold=fold, train_examples=train_examples)
 model = TabSTARClassifier(pretrain_dataset_or_path=dataset_id, device=device, verbose=False, random_state=SEED,
                           max_epochs=max_epochs)
