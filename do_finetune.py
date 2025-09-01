bd0f8eedc741a2b05239671f7daca730f6ac2b32
alanarazi7 <alanarazi7@gmail.com>
2025-09-01 19:36:39 +0300

    Moving last util

--- do_finetune.py
+++ do_finetune.py
@@ -1,4 +1,7 @@
 import argparse
+from dataclasses import dataclass
+from os.path import join
+from typing import Optional
 
 import torch
 import wandb
@@ -16,8 +19,42 @@ from tabstar_paper.pretraining.paths import get_model_path
 from tabstar_paper.pretraining.pretrain_args import PretrainArgs
 from tabstar_paper.utils.logging import wandb_run
 
-# TODO: remove tabular imports
-from tabular.trainers.finetune_args import FinetuneArgs
+# TODO: unsure this class is needed at all
+@dataclass
+class FinetuneArgs:
+    raw_exp_name: str
+    pretrain_args: PretrainArgs
+    lora_lr: float
+    lora_batch: int
+    lora_r: int
+    patience: int
+    epochs: int
+    keep_model: bool = False
+    full_exp_name: Optional[str] = None
+
+    def __post_init__(self):
+        self.full_exp_name = self.set_full_exp_name()
+
+    @classmethod
+    def from_args(cls, args: argparse.Namespace, exp_name: str, pretrain_args: PretrainArgs) -> Self:
+        return FinetuneArgs(raw_exp_name=exp_name,
+                            pretrain_args=pretrain_args,
+                            lora_lr=args.lora_lr,
+                            lora_batch=args.lora_batch,
+                            lora_r=args.lora_r,
+                            epochs=args.epochs,
+                            keep_model=args.downstream_keep_model,
+                            patience=args.patience)
+
+    def set_full_exp_name(self) -> str:
+        strings = [self.raw_exp_name,
+                   f"epochs_{self.epochs}",
+                   f"lora_lr_{self.lora_lr}",
+                   f"lora_batch_{self.lora_batch}",
+                   f"lora_r_{self.lora_r}",
+                   f"patience_{self.patience}"]
+        finetune_exp = "__".join(strings)
+        return join(self.pretrain_args.full_exp_name, finetune_exp)
 
 
 def finetune_tabstar(finetune_args: FinetuneArgs,
