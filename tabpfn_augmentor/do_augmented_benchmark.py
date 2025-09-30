bf685ee21b7c17e889292002fba0a6d9f97e2a2e
alanarazi7 <alanarazi7@gmail.com>
2025-09-30 14:10:35 +0300

    Turning to datasets that work

--- tabpfn_augmentor/do_augmented_benchmark.py
+++ tabpfn_augmentor/do_augmented_benchmark.py
@@ -10,18 +10,28 @@ from tabstar_paper.baselines.tabpfnv2 import TabPFNv2
 from tabstar_paper.baselines.xgboost import XGBoost
 
 augment_benchmark = [
-    # TODO: the code runs into a lot of edge-case errors when using other datasets...
+    OpenMLDatasetID.MUL_NATURE_YEAST_PROTEIN,
     OpenMLDatasetID.BIN_HEALTHCARE_BREAST_CANCER_WISCONSIN,
     OpenMLDatasetID.BIN_COMPUTERS_IMAGE_BANK_NOTE_AUTHENTICATION,
 
-    '''
-     File "/Users/alanarazi/tabstar_code/TabSTAR/.TabSTAR-env/lib/python3.11/site-packages/tabpfn_extensions/unsupervised/unsupervised.py", line 172, in impute_
-    pred = torch.stack([d for d in densities]).mean(dim=0)
-    '''
-    # OpenMLDatasetID.BIN_HEALTHCARE_BREAST_CANCER_WISCONSIN,
-    # OpenMLDatasetID.BIN_FINANCIAL_CREDIT_GERMAN,
+    # TODO: the code runs into a lot of edge-case errors when using some well known datasets... doing something wrong?
     # OpenMLDatasetID.REG_SPORTS_MONEYBALL,
+    # OpenMLDatasetID.BIN_FINANCIAL_CREDIT_GERMAN,
     # OpenMLDatasetID.MUL_NATURE_EUCALYPTUS_SEED,
+    '''
+      File "/Users/alanarazi/tabstar_code/TabSTAR/tabpfn_augmentor/generate_data.py", line 34, in augment_with_tabpfn
+        exp_synthetic.run(
+      File "/Users/alanarazi/tabstar_code/TabSTAR/.TabSTAR-env/lib/python3.11/site-packages/tabpfn_extensions/unsupervised/experiments.py", line 132, in run
+        self.synthetic_X = tabpfn.generate_synthetic_data(n_samples=n_samples, t=temp)
+                           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+      File "/Users/alanarazi/tabstar_code/TabSTAR/.TabSTAR-env/lib/python3.11/site-packages/tabpfn_extensions/unsupervised/unsupervised.py", line 410, in generate_synthetic_data
+        return self.impute_(
+               ^^^^^^^^^^^^^
+      File "/Users/alanarazi/tabstar_code/TabSTAR/.TabSTAR-env/lib/python3.11/site-packages/tabpfn_extensions/unsupervised/unsupervised.py", line 172, in impute_
+        pred = torch.stack([d for d in densities]).mean(dim=0)
+               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    TypeError: expected Tensor as element 0 in argument 0, but got numpy.ndarray
+    '''
 ]
 
 models = [XGBoost, RealMLP, TabPFNv2]
