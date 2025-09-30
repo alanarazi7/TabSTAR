683e15eedf4deb2d4f6e7e4d18ab754e2fd8957a
alanarazi7 <alanarazi7@gmail.com>
2025-09-30 15:47:07 +0300

    more datasets, log results

--- tabpfn_augmentor/do_augmented_benchmark.py
+++ tabpfn_augmentor/do_augmented_benchmark.py
@@ -14,16 +14,16 @@ from tabstar_paper.baselines.xgboost import XGBoost
 from tabstar_paper.leaderboard.data.keys import DATASET, MODEL, TEST_SCORE
 
 augment_benchmark = [
+OpenMLDatasetID.BIN_ANONYM_ALBERT,
+    OpenMLDatasetID.REG_ANONYM_HOUSE_16H,
     OpenMLDatasetID.BIN_ANONYM_CHRISTINE,
     OpenMLDatasetID.BIN_ANONYM_GINA,
     OpenMLDatasetID.BIN_ANONYM_MADELINE,
     OpenMLDatasetID.BIN_ANONYM_MADELONE,
-    OpenMLDatasetID.REG_HOUSES_CALIFORNIA_PRICES_2020,
-    OpenMLDatasetID.MUL_NATURE_YEAST_PROTEIN,
     OpenMLDatasetID.BIN_HEALTHCARE_BREAST_CANCER_WISCONSIN,
     OpenMLDatasetID.BIN_COMPUTERS_IMAGE_BANK_NOTE_AUTHENTICATION,
-    OpenMLDatasetID.BIN_ANONYM_ALBERT,
-    OpenMLDatasetID.REG_ANONYM_HOUSE_16H,
+    # OpenMLDatasetID.REG_HOUSES_CALIFORNIA_PRICES_2020,  # success
+    # OpenMLDatasetID.MUL_NATURE_YEAST_PROTEIN,     # error
 ]
 
 models = [XGBoost, TabPFNv2] #RealMLP, TabPFNv2]
