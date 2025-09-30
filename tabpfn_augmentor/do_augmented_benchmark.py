023439d34ffa1b40c5af0fd042d535902c67dae6
alanarazi7 <alanarazi7@gmail.com>
2025-09-30 13:19:56 +0300

    adding breast cancer

--- tabpfn_augmentor/do_augmented_benchmark.py
+++ tabpfn_augmentor/do_augmented_benchmark.py
@@ -10,12 +10,17 @@ from tabstar_paper.baselines.tabpfnv2 import TabPFNv2
 from tabstar_paper.baselines.xgboost import XGBoost
 
 augment_benchmark = [
+    OpenMLDatasetID.BIN_HEALTHCARE_BREAST_CANCER_WISCONSIN,
     OpenMLDatasetID.BIN_FINANCIAL_CREDIT_GERMAN,
     OpenMLDatasetID.REG_SPORTS_MONEYBALL,
     OpenMLDatasetID.MUL_NATURE_EUCALYPTUS_SEED,
 ]
 
 models = [XGBoost, RealMLP, TabPFNv2]
+
+num_folds = 5
+
+augmentations = [True, False]
 
 @dataclass
 class TabularTask:
@@ -26,7 +31,10 @@ class TabularTask:
 
 
 tabular_tasks = [TabularTask(dataset=d, model=m, fold=f, do_augment=a)
-                 for d in augment_benchmark for m in models for f in range(5) for a in [True, False]]
+                 for d in augment_benchmark
+                 for m in models
+                 for f in range(num_folds)
+                 for a in augmentations]
 
 
 def eval_augmented_benchmark():
